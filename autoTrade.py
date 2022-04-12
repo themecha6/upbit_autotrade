e05d2dc5a502fd53a50c95e618481b1ef835695f
themecha6 <103431707+themecha6@users.noreply.github.com>
2022-04-12 10:50:11 +0900

    Update autoTrade.py

--- autoTrade.py
+++ autoTrade.py
@@ -27,13 +27,13 @@ bid_price = 100000
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=1)
     start_time = df.index[0]
     return start_time
 
@@ -65,10 +65,10 @@ while True:
             
             now = datetime.datetime.now()
             start_time = get_start_time(tickers)
-            end_time = start_time + datetime.timedelta(days=1)
+            end_time = start_time + datetime.timedelta(hours=3, minutes=50)
             ma5 = get_yesterday_ma5(tickers)
 
-            if start_time < now < end_time - datetime.timedelta(seconds=10):
+            if start_time < now < end_time:
                 target_price = get_target_price(tickers, k)
                 current_price = get_current_price(tickers)
                 if (target_price < current_price) and (ma5 < current_price):
@@ -83,7 +83,7 @@ while True:
                     if balance * get_current_price(tickers) > 5000:
                         bid_tickers.append(tickers)
                         for tickers in bid_tickers:
-                            upbit.sell_market_order(tickers, balance*0.9995)
+                            upbit.sell_market_order(tickers, balance)
                             post_message(myToken,"#aleart", "{} 매도 완료, 매도 가격 : {}".format(tickers, balance))
             time.sleep(1)
     except Exception as e:
